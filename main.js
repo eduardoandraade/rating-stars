29971595bcf6bdc32ea04eb2fcee9e158ce8d801
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-04-14 15:00:04 -0300

    data set in localStorage

---
+++ main.js
@@ -0,0 +1,43 @@
+let stars = document.querySelectorAll(".ratings span");
+
+let products = document.querySelectorAll(".ratings");
+
+let ratings = [];
+
+for(let star of stars){
+    star.addEventListener("click", function(){
+        let children = star.parentElement.children;
+        for(let child of children){
+            if(child.getAttribute("data-clicked")){
+                return false;
+            }
+        }
+
+
+        this.setAttribute("data-clicked", "true")
+
+        let rating = this.dataset.rating;
+        let productId = this.parentElement.dataset.productid;
+        
+        let data = {
+            "stars": rating,
+            "product-id": productId
+        }
+
+        ratings.push(data);
+        localStorage.setItem("rating", JSON.stringify(ratings))
+    })
+}
+
+if(localStorage.getItem("rating")){
+
+    ratings = JSON.parse(localStorage.getItem("rating"))
+    for(let product of products){
+        if(ratings["product-id"] == product.dataset.productid){
+            let reversedStars = Array.from(product.children).reverse()
+            let index = parseInt(rating["stars"]) -1;
+
+            reversedStars[index].setAttribute("data-clicked", "true")
+        }
+    }
+}
